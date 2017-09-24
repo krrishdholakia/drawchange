dab5252faa13ba06855ecf440799b148d93f3ff0
vishalvijay30 <sinjaybrothers@gmail.com>
2017-09-24 19:36:46 -0400

    updated mongo collection

--- server.js
+++ server.js
@@ -29,5 +29,20 @@ console.log(url);
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to MongoDB!!");
-  db.close();
+  updateDocument(db, function() {
+    db.close();
+  });
 });
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+}
